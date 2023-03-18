0f893ef3e7fd0a719bd06bac6feebc3171f83f6a
ChillerK5 <klimbagr@mail.ru>
2023-03-18 22:40:58 +0700

    Add solution for problem-5

---
+++ problems-1/k-bagryantsev/problem5.py
@@ -0,0 +1,34 @@
+from math import sqrt, ceil
+from typing import Any, Generator
+
+
+def is_prime(a: int) -> bool:
+    for _i in range(2, ceil(sqrt(a))):
+        if a % _i == 0:
+            return False
+    return True
+
+
+def eratosthenes_sieve(_divisor: int) -> Generator[int, Any, None]:
+    while _divisor := _divisor + 1:
+        if is_prime(_divisor):
+            yield _divisor
+
+
+def factorizate_number(a: int) -> dict:
+    divisors = dict()
+    divisor = 2
+    while divisor <= a:
+        while a % divisor == 0:
+            if divisors.get(divisor) is None:
+                divisors[divisor] = 0
+            divisors[divisor] += 1
+            a //= divisor
+        divisor = next(eratosthenes_sieve(divisor))
+    return divisors
+
+
+if __name__ == '__main__':
+    num = int(input())
+    result = factorizate_number(num)
+    print([(key, value) for key, value in result.items()])
