8dee44a6b7cb47a599e3d9bdc53e47bf1beb57c8
ChillerK5 <klimbagr@mail.ru>
2023-05-29 05:01:31 +0700

    Add solution to problem3

---
+++ problems-2/k-bagryantsev/problem3.py
@@ -0,0 +1,22 @@
+import sys
+from operator import itemgetter
+from os import listdir, stat
+from os.path import isfile
+
+
+def print_dir_files(dirpath: str) -> None:
+    """Prints all dir files. Files are sorted by their size in descending order
+    and by name in ascending order.
+
+    Link to the documentation, which guarantees maintaining relative order of equal elements:
+    https://docs.python.org/3.11/library/functions.html#sorted"""
+    try:
+        files = filter(isfile, listdir(dirpath))
+        files = [(file, stat(file).st_size) for file in files]
+    except Exception as e:
+        raise e
+    print(*sorted(sorted(files, key=itemgetter(0)), key=itemgetter(1), reverse=True))
+
+
+if __name__ == '__main__':
+    print_dir_files(sys.argv[1])
