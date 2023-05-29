26302a4e691d1e86cc400a637c06feae0f88602c
ChillerK5 <klimbagr@mail.ru>
2023-05-29 10:07:59 +0700

    Add proper error handling

--- problems-2/k-bagryantsev/problem5.py
+++ problems-2/k-bagryantsev/problem5.py
@@ -1,13 +1,19 @@
 from collections.abc import Iterable
 from math import ceil, sqrt
+from sys import stderr
 
 
 def primes(bound: int) -> Iterable[int]:
-    """Calculates all prime numbers less than specified bound using Eratosthenes sieve."""
+    """Calculates all prime numbers less than specified bound using Eratosthenes sieve.
+
+    :argument bound -- positive integer bound
+    """
     if bound < 0:
         raise ValueError("Negative number")
+    if bound < 2:
+        return []
 
-    result = [True] * bound
+    result = [True] * (bound + 1)
     result[0] = False
     result[1] = False
 
@@ -24,4 +30,5 @@ if __name__ == '__main__':
         _n = int(input())
         print(*primes(_n))
     except Exception as e:
-        raise e
+        print(f"{e}\nExpected a positive integer", file=stderr)
+        exit()
